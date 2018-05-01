6ad96aed7ab584a55afc01a0022587167f553be0
djlaskarzewski <38737495+djlaskarzewski@users.noreply.github.com>
2018-05-01 12:51:12 -0400

    Add files via upload

---
+++ EmailClient.java
@@ -0,0 +1,214 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.*;
+import java.sql.SQLOutput;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class EmailClient extends JFrame implements ActionListener,EmailConstants {
+
+    //Server-Client variables
+   private BufferedReader br;
+   private PrintWriter pw;
+   private Socket s = null;
+    //Standard variables
+   private String host;
+   //Declaration of JFrame variables
+   private JTextField jtfTo;
+   private JTextField jtfFrom;
+   private JTextArea textArea = new JTextArea();
+
+   /*Main */
+   public static void main(String[] args) {
+      new EmailClient();
+   }
+
+
+   /*Constructor (Creates GUI)*/
+   public EmailClient() {
+      getContentPane().setLayout(new BorderLayout(0, 0));
+      this.setSize(950, 600);
+      this.setTitle("Email Client");
+      this.setLocationRelativeTo(null);
+      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+      JPanel inbox = new JPanel();
+      getContentPane().add(inbox,BorderLayout.WEST);
+      inbox.setLayout(new GridLayout(8, 0, 0, 0));
+   
+      JButton jbMsg;
+      jbMsg = new JButton("Open Inbox");
+      JButton jbConnect;
+      jbConnect = new JButton("Connect");
+      inbox.add(jbConnect);
+      inbox.add(jbMsg);
+   	
+      JPanel msgView = new JPanel();
+      getContentPane().add(msgView,BorderLayout.CENTER);
+      msgView.setLayout(new GridLayout(0, 1, 0, 0));
+      msgView.setBorder(new EmptyBorder(10, 10, 10, 10));
+   	
+      JPanel msgViewTop = new JPanel();
+      msgView.add(msgViewTop);
+      msgViewTop.setLayout(new GridLayout(0, 2, 0, 0));
+   	
+      JLabel jlTo = new JLabel("To: ");
+      jlTo.setHorizontalAlignment(SwingConstants.RIGHT);
+      msgViewTop.add(jlTo);
+   	
+      jtfTo = new JTextField(10);
+      msgViewTop.add(jtfTo);
+      jtfTo.setColumns(10);
+   	
+      JLabel jlFrom = new JLabel("From: ");
+      jlFrom.setHorizontalAlignment(SwingConstants.RIGHT);
+      msgViewTop.add(jlFrom);
+   	
+      jtfFrom = new JTextField();
+      msgViewTop.add(jtfFrom);
+      jtfFrom.setColumns(10);
+   	
+      JRadioButton jrbEncrypt = new JRadioButton("Encrypt Message");
+      msgViewTop.add(jrbEncrypt);
+   	
+      JLabel label = new JLabel("");
+      msgViewTop.add(label);
+   	
+      JLabel jlCompose = new JLabel("Compose: ");
+      msgViewTop.add(jlCompose);
+   	
+      JButton jbSend = new JButton("Send");
+      msgViewTop.add(jbSend);
+   	
+      JPanel msgViewBot = new JPanel();
+      msgView.add(msgViewBot);
+      msgViewBot.setLayout(new GridLayout(0, 1, 0, 0));
+   
+      msgViewBot.add(textArea);
+   	
+      this.setVisible(true);
+   
+      jbConnect.addActionListener(this);
+      jbMsg.addActionListener(this);
+      jbSend.addActionListener(this);
+   
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent ae) {
+   
+      switch (ae.getActionCommand()){
+         case "Connect":
+         
+            System.out.println("I got in the connect case of the client");
+            host = JOptionPane.showInputDialog(this,"Enter the IP address of the server",null);
+            
+            try{
+               s = new Socket(host,42069);
+               br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+               pw = new PrintWriter(new PrintWriter(s.getOutputStream()));
+            }
+            catch(UnknownHostException uhe){
+               System.out.println("Unknown Host Exception caught");
+               uhe.printStackTrace();
+            }
+            catch (IOException ioe){
+               System.out.println("IO exception caught");
+               ioe.printStackTrace();
+            }
+            catch (Exception e){
+               System.out.println("Some other exception caught!");
+               e.printStackTrace();
+            }
+            try{
+               String p = br.readLine();
+               if(p.equals("Login required")){
+                  String name = JOptionPane.showInputDialog("Enter username: ");
+               
+                  System.out.println("The client received the name " + name + " from the login frame");
+                  pw.println(name);
+                  pw.flush();
+               
+                  String ok = br.readLine();
+                  System.out.println(ok + " ok");
+                  if(ok.equals("Ok")){
+                     System.out.println("name valid");
+                  }
+               
+               }
+            }catch(Exception e){
+            
+            }
+            break;
+      
+         case "Send":
+            System.out.println("Send case reached");
+
+            try {
+             pw.println("Send");
+             pw.flush();
+             if(br.readLine().substring(0,3).equals("220")){
+                  
+               pw.println("HELO");
+               pw.flush();
+               System.out.println("HELO");
+               
+               if(br.readLine().substring(0,3).equals("250")){
+                  pw.println("MAIL FROM:<" +jtfFrom.getText() + ">");
+                  pw.flush();
+                  System.out.println("MAIL FROM");
+                  if(br.readLine().substring(0,3).equals("250")){
+                     pw.println("RCPT TO:<" +jtfTo.getText() + ">");
+                     pw.flush();
+                     System.out.println("RCPT TO");
+                     if(br.readLine().substring(0,3).equals("250")){
+                        pw.println("DATA");
+                        pw.flush();
+                        System.out.println("DATA");
+                        String three = br.readLine();
+                        System.out.println(three);
+                           
+                        if(three.substring(0,3).equals("354")){
+                           System.out.println("got 354");
+                           for(String s : textArea.getText().split("\\n")){
+                              pw.println(s);
+                              pw.flush();
+                           }
+                           if(br.readLine().substring(0,3).equals("250")){
+                              pw.println("QUIT");
+                              pw.flush();
+                              System.out.println("QUIT");
+                              if(br.readLine().substring(0,3).equals("221")){
+                                 System.out.println("Victory");
+                              }
+                              
+                           }
+                           
+                        }
+                        
+                     }
+                  }
+                  
+               }
+               
+            }  
+            } catch(IOException ioe){
+               System.out.println(ioe);
+            }
+            
+            break;
+         case "Open Inbox":
+            System.out.println("In the 'open' case");
+            pw.println("Show mail");
+            pw.flush();
+            break;
+      
+      }
+   }
+}
---
+++ EmailServer.java
@@ -0,0 +1,287 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.*;
+import java.io.*;
+import java.nio.file.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+
+public class EmailServer extends JFrame implements ActionListener,EmailConstants {
+
+   //Mailboxes
+   private ArrayList<Mailbox> boxes = new ArrayList<>();
+   private Mailbox alex = new Mailbox("alex");
+   private Mailbox alex2 = new Mailbox("dan");
+   private Mailbox alex3 = new Mailbox("mike");
+   private Mailbox alex4 = new Mailbox("hamis");
+   private Mailbox guest = new Mailbox("guest");
+   final String dir = System.getProperty("user.dir");
+
+
+   //GUI components
+   private JButton jbStart = new JButton("Start Server");
+   private BufferedReader br = null;
+   private PrintWriter pw = null;
+
+   public static void main(String[] args){
+      new EmailServer();
+   }//end main
+
+   /**
+    * Email Server constructor
+    */
+   private EmailServer(){
+      this.setSize(400,200);
+      this.setLocation(200,400);
+      this.setTitle("Email Server");
+      this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+   
+      JPanel jpNorth = new JPanel(new FlowLayout());
+      jpNorth.add(jbStart);
+      jbStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+   
+      this.add(jpNorth,BorderLayout.NORTH);
+      this.setVisible(true);
+   
+   
+      jbStart.addActionListener(this);
+      System.out.println("ActionLister added to jbStart");
+      boxes.add(alex);
+      boxes.add(alex2);
+      boxes.add(alex3);
+      boxes.add(alex4);
+      boxes.add(guest);
+   }//end constructor
+
+   /**
+    *@param ae takes the actionPerformed from the various buttons
+    */
+   @Override
+   public void actionPerformed(ActionEvent ae) {
+   
+      switch (ae.getActionCommand()) {
+         case "Start Server":
+            System.out.println("I got to the start case of the start button server");
+            jbStart.setText("Stop Server");
+            Thread t = new Thread(
+               ()-> {
+                  try{
+                     ServerSocket ss; //= null
+                     System.out.println("Created server socket");
+                     ss = new ServerSocket(PORT);
+                  
+                     Socket soc; // = null
+                     while (true){
+                        System.out.println("Entered infinite while; waiting for connections");
+                        soc = ss.accept();
+                        System.out.println("Connected to: " + soc.getInetAddress().getHostAddress());
+                     
+                     //Thread created
+                        ThreadedServer ths = new ThreadedServer(soc);
+                        System.out.println("Threaded Server created");
+                        ths.start();
+                        System.out.println("Threaded server started");
+                     }
+                  }
+                  catch (IOException ioe){
+                     System.out.println("IOException caught when creating a new server thread");
+                     ioe.printStackTrace();
+                  }
+               });
+         
+            t.start();
+      }
+   }
+
+   class ThreadedServer extends Thread {
+   
+      Socket client; //make null
+   
+      /**
+       * @param inSoc takes socket to which clients can connect
+       */
+      ThreadedServer(Socket inSoc){
+         this.client = inSoc;
+         try{
+            System.out.println("I am in the constructor of the Threaded Server");
+            br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            pw = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+         }
+         catch (IOException ioe){
+            System.out.println("IOException caught while creating br and pw");
+            ioe.printStackTrace();
+         }
+      }//end ThreadedServer constructor
+   
+      /**
+       * Run method of the ThreadedServer class
+       */
+      public void run(){
+         System.out.println("Connected to " + client.getInetAddress().getHostAddress());
+         String confirmed ="";
+         try {
+            pw.println("Login required");
+            pw.flush();
+            String name = br.readLine();
+            System.out.println("The name received from the client is " + name);
+            Account act = new Account();
+            if(act.LogInUser(name)){
+               System.out.println("User " + name + " logged in.");
+               confirmed = name;
+               pw.println("Ok");
+               pw.flush();
+               
+               
+               System.out.println("The name confirmed is " + confirmed);
+               
+            
+               File f = new File(dir + File.separator +  confirmed + ".txt");
+               System.out.println(f);
+                  
+               String cmd = br.readLine();
+               if (cmd.equals("Send")){
+                  if(confirmed.equals(alex.getName())){
+                     System.out.println("I am in the if 'confirmed == alex.getName()' ");
+                     smtp(confirmed, f);
+                  
+                  }
+                  else if(confirmed.equals(alex2.getName())){
+                     System.out.println("I am in the if 'confirmed == alex2.getName()' ");
+                     smtp(confirmed, f);
+                  }
+                  else if(confirmed.equals(alex3.getName())){
+                     System.out.println("I am in the if 'confirmed == alex3.getName()' ");
+                     smtp(confirmed, f);
+                  
+                  }
+                  else if(confirmed.equals(alex4.getName())){
+                     System.out.println("I am in the if 'confirmed == alex4.getName()' ");
+                     smtp(confirmed, f);     
+                  }
+                  else if(confirmed.equals(guest.getName())){
+                     System.out.println("I am in the if 'confirmed == guest.getName()' ");
+                     smtp(confirmed, f);
+                  }
+               }
+               //SHOW INBOX
+               if (cmd.equals("Show mail")){
+                  System.out.println(f.getPath());
+                  Desktop.getDesktop().open(f);
+               }
+               else{
+                  JOptionPane.showMessageDialog(jbStart,"Oops, something went wrong. ");
+               
+               }
+            }
+            else{
+               JOptionPane.showMessageDialog(jbStart,"Guests login with \"guest\"");
+               
+            }
+         
+         } catch (IOException ioe) {
+            ioe.printStackTrace();
+         }
+         try{
+            br.close();
+            pw.close();
+         }
+         catch (IOException ioe){
+            ioe.printStackTrace();
+         }
+      
+      
+      }
+   }
+   
+   public void smtp(String confirmed, File f) {
+      try {
+         Message m = new Message();
+         pw.println("220");
+         pw.flush();
+         if(br.readLine().substring(0,4).equals("HELO")){
+            System.out.println("got to HELO");
+            pw.println("250");
+            pw.flush();
+            String from = br.readLine();
+            if(from.substring(0,9).equals("MAIL FROM")){
+               m.setFrom(from.substring(from.indexOf('<'), from.length() -1));
+               pw.println("250");
+               pw.flush();
+               String to = br.readLine();
+               if(to.substring(0,7).equals("RCPT TO")){
+                  m.setTo(to.substring(to.indexOf('<'), to.length() -1));
+                  pw.println("250");
+                  pw.flush();
+                  System.out.println("sent here");
+                  if(br.readLine().substring(0,4).equals("DATA")){
+                     pw.println("354");
+                     pw.flush();
+                     ArrayList<String> msg = new ArrayList<String>();
+                     boolean loop = true;
+                     while(loop){
+                        String line = br.readLine();
+                        if(!line.equals("."))
+                           msg.add(line);
+                        else {
+                           loop = false;
+                           String message = "";
+                           for(String s : msg){
+                              message+=s;
+                           }
+                           m.setMessage(message);
+                           System.out.println("Message: " + m.getMessage());
+                           pw.println("250");
+                           pw.flush();
+                           if(br.readLine().substring(0,4).equals("QUIT")){
+                              pw.println("221");
+                              pw.flush();
+                           }
+                        }
+                     }
+                                                  
+                  }
+                     
+               }
+            }
+               
+         }
+      
+            
+         BufferedWriter bwFile = new BufferedWriter(new FileWriter(f,true));
+            
+         bwFile.write("From: " + m.getFrom());
+         bwFile.flush();
+         bwFile.newLine();
+            
+         bwFile.write("To: " + m.getTo());
+         bwFile.flush();
+         bwFile.newLine();
+            
+         bwFile.write("Message\n: " + m.getMessage());
+         bwFile.flush();
+         bwFile.newLine();
+            
+         alex.add(m);
+            
+         for(Mailbox mb: boxes){
+            System.out.println("Name of the mailbox "+ mb.getName()+"\n");
+            for( Message ms : mb ){
+               if(ms != null) {
+                  System.out.println("Message inside mailbox" + ms.getMessage());
+                  System.out.println("From: " + ms.getFrom());
+                  System.out.println("Sent to: " + ms.getTo());
+               }
+            }
+               
+         }
+      } catch(IOException ioe){
+         System.out.println(ioe);
+      }
+   }
+}
+//login
+//send request
+//client requests message download
+//send messages from server to server
